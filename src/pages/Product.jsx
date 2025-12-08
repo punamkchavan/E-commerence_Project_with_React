02c2fe4447ebf3ab82ca73ac752d6dbaf83908ed
punamkchavan <cpunam1123@gmail.com>
2025-12-08 16:11:28 +0530

    update product file

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,32 @@
 
 import { Figure, Button} from "react-bootstrap"
 import '../css/style.css'
+import axios from 'axios';
+import { useState, useEffect } from "react";
 
 export default function Product(){
+
+    const [data,setData]=useState([]);
+
+    const API="https://fakestoreapi.com/products"
+
+    const FetchData= async()=>{
+            try{
+
+                const Response= await axios(API);
+                console.log(Response.data)
+                setData(Response.data)
+
+            }catch(error){
+                console.log(error)
+            }
+    }
+
+    useEffect(()=>{
+         FetchData();
+
+    },[])
+
     return(
         
     <div>
@@ -11,11 +35,12 @@ export default function Product(){
         width={290}
         height={150}
         alt="product"
-        src="image.png"
+        src=""
       />
       <div className="container2">
-      <h3>Product Name</h3>    
-       <p>Price Rs.</p>
+        <h2>ID:{data.id}</h2>
+      <h3>Product Name:{data.title}</h3>    
+       <p>Price Rs.:{data.count}</p>
       <Button>Add to Cart</Button>
       </div>
     </Figure>
